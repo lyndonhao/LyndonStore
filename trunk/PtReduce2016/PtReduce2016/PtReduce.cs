24c40fadb6ea075aaec7ad4c9ab149f584add018
js.cheng <js.cheng@39310c1a-5cc6-f14a-9316-4f50c5761e00>
2016-03-29 09:04:56 +0000

    1、编辑查找字符串函数。

--- trunk/PtReduce2016/PtReduce2016/PtReduce.cs
+++ trunk/PtReduce2016/PtReduce2016/PtReduce.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
+using DataProcessTools;
 
 namespace PtReduce2016
 {
@@ -20,6 +22,18 @@ namespace PtReduce2016
         private void PtReuce_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataProcessTools.ParseData a = new DataProcessTools.ParseData();
+            string b="MOVEJ/-102.709,82.550,-54.028,140.990,112.893,-17.491/-60.284,-10.000,1.000,180.000,0.000,-50.501/";
+            //int[] n=new 
+            //ArrayList dsd =  new ArrayList();
+            //dsd.Count
+           // List<string> ddd= new List<string>;
+            List<int> n = new List<int>();
+            bool c=a.FindString("/",b,ref n);
         }
     }
 }
