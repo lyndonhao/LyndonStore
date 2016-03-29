e2d0f5e9f39d61c1f12cf70833fc342aa331c35a
js.cheng <js.cheng@39310c1a-5cc6-f14a-9316-4f50c5761e00>
2016-03-29 02:33:14 +0000

    程序分为输入，解析，处理，输出四部分 1、读取Txt函数。

--- PtReduce2016/DataProcessTools/ParseData.cs
+++ PtReduce2016/DataProcessTools/ParseData.cs
@@ -8,5 +8,10 @@ namespace DataProcessTools
 {
     public class ParseData
     {
+        public bool FindString(string x_sIdentification,string x_sTargetString)
+        {
+            bool l_bResult = false;
+            return l_bResult;
+        }
     }
 }
---
+++ PtReduce2016/DocTools/fileRead.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileTools
+{
+    class fileRead
+    {
+        /// <summary>
+        /// 读取txt文件，将txt中的每行保存到string数组中。
+        /// </summary>
+        /// <param name="x_sPath"></param>
+        /// <returns></returns>
+        public string[] ReadTxt(string x_sPath)
+        {
+            string l_sLine=string.Empty;
+            StreamReader SR = new StreamReader(x_sPath,Encoding.Default);
+            string[] l_sFileInfor = new string[5000];
+
+            int i = 0;
+
+
+            while ((l_sLine = SR.ReadLine()) != null)
+            {
+                l_sFileInfor[i] = l_sLine.ToString();
+                i++;
+            }
+            return l_sFileInfor;
+
+        }
+    }
+}
