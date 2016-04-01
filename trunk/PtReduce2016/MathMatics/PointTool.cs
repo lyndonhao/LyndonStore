ac4953597292a241b90c64b9c7720bfd222d2544
js.cheng <js.cheng@39310c1a-5cc6-f14a-9316-4f50c5761e00>
2016-04-01 08:00:11 +0000

    1、增加矩阵运算类。 2、三点建圆功能。

---
+++ trunk/PtReduce2016/MathMatics/PointTool.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMatics
+{
+    public class PointTool
+    {
+        /// <summary>
+        /// 判断三点共线
+        /// </summary>
+        /// <returns>true：共线</returns>false：不共线
+        public static bool ThreeColline(DataType.BasicDataType.vector x_vVector1, DataType.BasicDataType.vector x_vVector2, DataType.BasicDataType.vector x_vVector3)
+        {
+            bool l_bOk = false;
+            DataType.BasicDataType.vector l_V1, l_V2;
+            l_V1 =BasicMathTool.SubVector(x_vVector1, x_vVector2);
+            l_V2 =BasicMathTool.SubVector(x_vVector2, x_vVector3);
+            if (l_V2.x * l_V1.y == l_V1.x * l_V2.y & l_V2.x * l_V1.z == l_V2.z * l_V1.x)
+            {
+                l_bOk = true;
+            }
+            else
+            {
+                l_bOk = false;
+            }
+            return l_bOk;
+        }
+
+        public static bool CreatCircle(DataType.BasicDataType.vector x_vVector1, DataType.BasicDataType.vector x_vVector2, DataType.BasicDataType.vector x_vVector3, out DataType.BasicDataType.vector x_vCenterPoint, out double x_nRadius)
+        {
+            x_vCenterPoint.x = 0;
+            x_vCenterPoint.y = 0;
+            x_vCenterPoint.z = 0;
+            x_nRadius = 0;
+
+            bool l_bOk = false;
+            l_bOk = ThreeColline(x_vVector1, x_vVector2, x_vVector3);      
+            if (l_bOk == false)
+            {
+                double[,] l_nMatrix = new double[3, 3];
+                l_nMatrix[0, 0] = (x_vVector1.y - x_vVector3.y) * (x_vVector2.z - x_vVector3.z) - (x_vVector2.y - x_vVector3.y) * (x_vVector1.z - x_vVector3.z);
+                l_nMatrix[0, 1] = (x_vVector2.x - x_vVector3.x) * (x_vVector1.z - x_vVector3.z) - (x_vVector1.x - x_vVector3.x) * (x_vVector2.z - x_vVector3.z);
+                l_nMatrix[0, 2] = (x_vVector1.x - x_vVector3.x) * (x_vVector2.y - x_vVector3.y) - (x_vVector2.x - x_vVector3.x) * (x_vVector1.y - x_vVector3.y);
+                l_nMatrix[1, 0] = x_vVector2.x - x_vVector1.x;
+                l_nMatrix[1, 1] = x_vVector2.y - x_vVector1.y;
+                l_nMatrix[1, 2] = x_vVector2.z - x_vVector1.z;
+                l_nMatrix[2, 0] = x_vVector3.x - x_vVector2.x;
+                l_nMatrix[2, 1] = x_vVector3.y - x_vVector2.y;
+                l_nMatrix[2, 2] = x_vVector3.z - x_vVector2.z;
+                double[] l_Matrix_b = new double[3];
+                l_Matrix_b[0] = l_nMatrix[0, 0] * x_vVector3.x + l_nMatrix[0, 1] * x_vVector3.y + l_nMatrix[0, 2] * x_vVector3.z;
+                l_Matrix_b[1] = (l_nMatrix[1, 0] * (x_vVector2.x + x_vVector1.x) + l_nMatrix[1, 1] * (x_vVector2.y + x_vVector1.y) + l_nMatrix[1, 2] * (x_vVector2.z + x_vVector1.z)) / 2;
+                l_Matrix_b[2] = (l_nMatrix[2, 0] * (x_vVector3.x + x_vVector2.x) + l_nMatrix[2, 1] * (x_vVector3.y + x_vVector2.y) + l_nMatrix[2, 2] * (x_vVector3.z + x_vVector2.z)) / 2;
+                double[,] l_inverse = new double[3, 3];
+                l_bOk = MatrixTool.MatrixInverse(l_nMatrix, ref l_inverse);
+                if (l_bOk == true)
+                {
+                    double[] l_CenterPoint = new double[3];
+                    l_bOk = MatrixTool.MatrixDotMultiply(l_inverse, l_Matrix_b, out l_CenterPoint);
+                    if (l_bOk == true)
+                    {
+                        x_vCenterPoint.x = l_CenterPoint[0];
+                        x_vCenterPoint.y = l_CenterPoint[1];
+                        x_vCenterPoint.z = l_CenterPoint[2];
+                        x_nRadius = Math.Sqrt(Math.Pow(x_vVector1.x - x_vCenterPoint.x, 2) + Math.Pow(x_vVector1.y - x_vCenterPoint.y, 2) + Math.Pow(x_vVector1.z - x_vCenterPoint.z, 2));
+                        
+                    }
+                }
+            }
+            return l_bOk;
+        }
+    }
+}
--- trunk/PtReduce2016/PtReduce2016/PtReduce.cs
+++ trunk/PtReduce2016/PtReduce2016/PtReduce.cs
@@ -12,6 +12,7 @@ using System.Windows.Forms;
 using System.Collections;
 using DataProcessTools;
 using DataType;
+using MathMatics;
 
 namespace PtReduce2016
 {
@@ -33,6 +34,39 @@ namespace PtReduce2016
             string b="MOVEJ/-102.709,82.550,-54.028,140.990,112.893,-17.491/-60.284,-10.000,1.000,180.000,0.000,-50.501/";
             DataType.StaubliRobotData.St_PointRx pPoint=new DataType.StaubliRobotData.St_PointRx();
             bool l_bOk=DataProcessTools.ParseData.getPoint("/",',',b,pPoint);
+
+
+            double[,] matrix_a = new double[3, 3];
+            matrix_a[0, 0] = 1;
+            matrix_a[0, 1] = 2;
+            matrix_a[0, 2] = 3;
+            matrix_a[1, 0] = 5;
+            matrix_a[1, 1] = 6;
+            matrix_a[1, 2] = 7;
+            matrix_a[2, 0] = 9;
+            matrix_a[2, 1] = 8;
+            matrix_a[2, 2] = 3;
+            DataType.BasicDataType.vector[] abc=new DataType.BasicDataType.vector[4];
+            double r=new double();
+            abc[0].x=1;
+            abc[0].y=2;
+            abc[0].z=3;
+
+            abc[1].x=5;
+            abc[1].y=6;
+            abc[1].z=7;
+
+            abc[2].x=9;
+            abc[2].y=8;
+            abc[2].z=3;
+
+
+            l_bOk = PointTool.CreatCircle(abc[0], abc[1], abc[2],out abc[3],out r);
+
+            double[,] matrix_b=new double[3,3];
+            double c=MatrixTool.MatrixSurplus(matrix_a);
+            l_bOk = MatrixTool.MatrixInverse(matrix_a,ref matrix_b);
+            matrix_b = MatrixTool.MatrixTranspose(matrix_a);
             
             //int[] n=new 
             //ArrayList dsd =  new ArrayList();
